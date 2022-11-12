4406e9c15e7f74e829ec1f82d73f1e22e83ce5b7
Slogllykop <slogllykop07@gmail.com>
2022-11-12 20:33:03 +0530

    1

---
+++ FDS/Group_A/1.py
@@ -0,0 +1,9 @@
+# PyCharm Installation and running addition of 2 numbers using Python
+
+def addition(num1, num2):
+    return num1 + num2
+
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+print('Sum of two numbers is: ', addition(num1, num2))
